d354ea6e02280e3463d781604d1fa02cac81ce4c
venu ajitesh <venuajitesh@gmail.com>
2022-07-08 01:09:00 +0530

    Commited2

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "https://47ae5a87.konnect-friends.pages.dev/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
